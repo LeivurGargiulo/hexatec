14a97acba2af78859e4d1167ffa53471f047f7d5
LeivurGargiulo <leivurgargiulo@gmail.com>
2025-07-26 04:46:20 -0300

    third commit

---
+++ src/scripts/aos.js
@@ -0,0 +1,8 @@
+import AOS from "aos";
+
+AOS.init({
+  disable: "mobile", // disable on mobile devices
+  duration: 800, // animation duration
+  once: false, // animate again on scroll up
+  offset: 100, // trigger position
+});
---
+++ src/scripts/lenis.js
@@ -0,0 +1,19 @@
+import Lenis from "@studio-freight/lenis";
+
+const isMobile = window.innerWidth < 768
+
+if (!isMobile) {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+    smoothTouch: false,
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
+}
